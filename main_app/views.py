a6b3a29119a75f17aa0b1142ebde62844c402d77
Zecrim <samuel.thomas424@gmail.com>
2024-06-27 11:28:24 -0500

    adding recipes from the api is working

--- main_app/views.py
+++ main_app/views.py
@@ -7,7 +7,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 import requests
-from django.http import JsonResponse
 from .models import Recipe
 from .forms import RecipeSearchForm
 
@@ -47,32 +46,30 @@ def recipe_search(request):
                 results = get_api_data(query)
                 if 'error' not in results and results.get('results'):
                     recipes = results['results']
-
+                    # Save the recipes to the session
+                    request.session['recipes'] = recipes
         elif 'add-button' in request.POST:
-            recipe_id = request.POST.get('id')
-            print (recipe_id)
-            results = get_api_data('')
-            if 'error' not in results and results.get('results'):
-                for recipe_data in recipes:
-                    if str(recipe_data['id']) == recipe_id:
-                        # recipe_data = recipe_data
-                        name = recipe_data.get('title', 'No Title')
-                        cuisine = recipe_data.get('cuisines', '')[0]
-                        ingredients = '\n'.join([ingredient['original'] for ingredient in recipe_data.get('extendedIngredients', [])])
-                        instructions = '\n'.join([step['step'] for instruction in recipe_data.get('analyzedInstructions', []) for step in instruction.get('steps', [])])
-                        image = recipe_data.get('image', '')
-                        user = request.user
+            recipe_id = request.POST.get('recipe_id')
+            recipes = request.session.get('recipes', [])
+            for recipe_data in recipes:
+                if str(recipe_data['id']) == recipe_id:
+                    name = recipe_data.get('title', 'No Title')
+                    cuisine = recipe_data.get('cuisines', [''])[0]
+                    ingredients = '\n'.join([ingredient['original'] for ingredient in recipe_data.get('extendedIngredients', [])])
+                    instructions = '\n'.join([f"{i+1}. {step['step']}" for i, step in enumerate([step for instruction in recipe_data.get('analyzedInstructions', []) for step in instruction.get('steps', [])])])
+                    image = recipe_data.get('image', '')
+                    user = request.user
 
-                        Recipe.objects.create(
-                            name=name,
-                            cuisine=cuisine,
-                            ingredients=ingredients,
-                            instructions=instructions,
-                            image=image,
-                            user=user
-                        )
+                    Recipe.objects.create(
+                        name=name,
+                        cuisine=cuisine,
+                        ingredients=ingredients,
+                        instructions=instructions,
+                        image=image,
+                        user=user
+                    )
 
-                        return redirect('recipe-index')
+                    return redirect('recipe-index')
 
     return render(request, 'main_app/recipe_search.html', {'form': form, 'recipes': recipes})
 
@@ -84,17 +81,10 @@ def recipe_index(request):
 @login_required
 def recipe_detail(request, recipe_id):
     recipe = Recipe.objects.get(id=recipe_id)
-    # dlcs_game_doesnt_have = Dlc.objects.exclude(id__in = game.dlcs.all().values_list('id'))
     return render(request, 'recipes/detail.html', {
         'recipe': recipe,
-        # 'dlcs': dlcs_game_doesnt_have
     })
 
-
-# @login_required
-# def associate_dlc(request, game_id, dlc_id):
-#     Game.objects.get(id=game_id).dlcs.add(dlc_id)
-#     return redirect('game-detail', game_id=game_id)
 
 def signup(request):
     # Handle the POST request
@@ -102,8 +92,8 @@ def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save() # STEP 1: Create a user in the databse from the UserCreationForm
-            login(request, user) # STEP 2: Log in as the craeted user
+            user = form.save()
+            login(request, user)
             return redirect('recipe-index')
         else:
             error_message = 'Invalid sign up - try again'
@@ -116,13 +106,6 @@ class RecipeCreate(LoginRequiredMixin, CreateView):
     model = Recipe
     fields = ['name', 'cuisine', 'ingredients', 'instructions']
     success_url = '/recipes'
-    
-    def form_valid(self, form):
-        # Assign the logged in user (self.request.user)
-        form.instance.user = self.request.user
-        form.instance.ingredients = add_checkbox_to_lines(form.cleaned_data['ingredients'])
-        # Let the CreateView do its job as usual
-        return super().form_valid(form)
 
 
 class RecipeUpdate(LoginRequiredMixin,UpdateView):
@@ -132,24 +115,6 @@ class RecipeUpdate(LoginRequiredMixin,UpdateView):
 class RecipeDelete(LoginRequiredMixin,DeleteView):
     model = Recipe
     success_url = '/recipes'
-
-# class DlcCreate(LoginRequiredMixin,CreateView):
-#     model = Dlc
-#     fields = '__all__'
-
-# class DlcList(LoginRequiredMixin,ListView):
-#     model = Dlc
-
-# class DlcDetail(LoginRequiredMixin,DetailView):
-#     model = Dlc
-
-# class DlcUpdate(LoginRequiredMixin,UpdateView):
-#     model = Dlc
-#     fields = ['name', 'color']
-
-# class DlcDelete(LoginRequiredMixin,DeleteView):
-#     model = Dlc
-#     success_url = '/dlcs/'
 
 class Home(LoginView):
     template_name = 'home.html'
